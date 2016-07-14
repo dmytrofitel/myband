75ab23214ba917c49a33d4f2f33ffc87e451dcff
dmytrofitel <dimfit@gmail.com>
2016-07-14 12:50:47 +0000

    add new file

---
+++ assets/js/contact.js
@@ -0,0 +1,19 @@
+/*global $*/
+$(function(){
+  $('#send').click(function(e){
+      e.preventDefault();
+      $.ajax({
+        url: "https://formspree.io/dimfit@gmail.com", 
+        method: "POST",
+        data: {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                subject: $('#subject').val(),
+                message: $('#message').val(),
+        },
+        dataType: "json"
+    }).done(function(){
+        $('#form').html('<h1>Thank You</h1>');
+    });
+  });
+});
